a6441783665b72513d3cd4f57bb5f92b8d839f42
shrutidaundkar <shrutivd135@gmail.com>
2023-04-15 15:52:46 -0700

    Updated HW1 according to the reviews

--- hw1/01-selectors.md
+++ hw1/01-selectors.md
@@ -11,8 +11,8 @@
 9. plate,bento
 10. \*
 11. plate \*
-12. plate + .small, plate + apple
-13. pickle ~ pickle
+12. plate + apple
+13. bento ~ pickle
 14. plate > apple
 15. orange:first-child
 16. apple,plate pickle:only-child
@@ -26,9 +26,9 @@
 24. orange:last-of-type, apple:last-of-type
 25. bento:empty
 26. :not(#fancy, .small, plate:nth-last-child(2), plate)
-27. apple[for], plate[for], bento[for]
+27. [for]
 28. plate[for]
 29. bento[for="Vitaly"]
-30. [for^="Sam"], [for^="Sarah"]
-31. [for$="Hayato"], [for$="Minato"]
-32. [for*="Bobby"], [for*="Robbie"]
+30. [for^="Sa"]
+31. [for$="ato"]
+32. [for*="obb"]
--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,23 +1,27 @@
-const elem = document.querySelector('input');
-const resultText = document.getElementById('resultText');
-elem.addEventListener('input', handleInput);
+const elem = document.querySelector("input");
+const resultElement = document.querySelector("#resultElement");
+elem.addEventListener("input", handleInput);
 
-function handleInput(){
-    let input = parseInt(elem.value);
-    if(input >= 0){
-        let reverseInput = parseInt(String(elem.value).split('').reverse().join(''));
-        if( input == reverseInput){
-            resultText.style.color = 'green';
-            resultText.innerHTML = 'Yes. This is a palindrome!';
-        }else{
-            resultText.style.color = 'red';
-            resultText.innerHTML = 'No. Try again.'
-        }
-    }else if(input<0){
-        resultText.style.color = 'red';
-        resultText.innerHTML = 'Enter a positive number!';
-    }else{
-        resultText.style.color = 'black';
-        resultText.innerHTML = 'Please enter a number first!';
+function handleInput() {
+  const input = parseInt(elem.value);
+  if (input >= 0) {
+    const reverseInput = parseInt(
+      String(elem.value).split("").reverse().join("")
+    );
+    if (input === reverseInput) {
+      updateResultElement("text-success", "Yes. This is a palindrome!");
+    } else {
+      updateResultElement("text-danger", "No. Try again.");
     }
+  } else if (input < 0) {
+    updateResultElement("text-danger", "Enter a positive number!");
+  } else {
+    updateResultElement("text-black", "Please enter a number first!");
+  }
+}
+
+function updateResultElement(resultColorClass, resultTextString) {
+  resultElement.classList = "";
+  resultElement.classList.add(resultColorClass);
+  resultElement.textContent = resultTextString;
 }
