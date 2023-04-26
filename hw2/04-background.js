ec940839a8f151800f2e24fbfaf458f5b39ebc52
shrutidaundkar <shrutivd135@gmail.com>
2023-04-26 16:56:03 -0700

    Uploading exercises 4-5

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,42 @@
 // Add your code here
+document.addEventListener("DOMContentLoaded", (event) => {
+  const startStopButton = document.querySelector("#startStopButton");
+  const intervalInput = document.querySelector("#intervalInput");
+  let currentInterval;
+  let userIntervalUpdated = false;
+
+  const generateColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    const a = 0.4;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+  };
+  const updateColor = () => {
+    document.body.style.backgroundColor = generateColor();
+  };
+  const start = (interval) => {
+    currentInterval = setInterval(updateColor, interval * 1000);
+  };
+  const stop = () => {
+    clearInterval(currentInterval);
+  };
+
+  const startStopToggle = () => {
+    if (userIntervalUpdated) {
+      stop();
+      startStopButton.value = "Start";
+      startStopButton.classList.add("btn-primary");
+      startStopButton.classList.remove("btn-danger");
+    } else {
+      start(intervalInput.value);
+      startStopButton.value = "Stop";
+      startStopButton.classList.remove("btn-primary");
+      startStopButton.classList.add("btn-danger");
+    }
+    userIntervalUpdated = !userIntervalUpdated;
+  };
+
+  startStopButton.addEventListener("click", startStopToggle);
+  startStopToggle();
+});
