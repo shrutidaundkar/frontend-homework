1e7e20e7c1750001b40e397add8751ce21e484ce
shrutidaundkar <shrutivd135@gmail.com>
2023-04-27 01:27:41 -0700

    Exercise 3 updated

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,21 @@
-// Add your code here
+// // Add your code here
+const inputWord = document.querySelector("#inputWord");
+const textContainer = document.querySelector("#textContainer");
 
-input.addEventListener('keydown', handleKeyDown);
+inputWord.addEventListener("keyup", () => {
+  const searchText = inputWord.value.trim();
+  const textContent = textContainer.textContent;
+  if (searchText != "") {
+    const wordsArray = textContent.split(" ");
+    const highlightedWordsArray = wordsArray.map((word) => {
+      const trimmedWord = word.replace(/[^\w\s]/gi, "");
+      return trimmedWord === searchText
+        ? `<span class="bg-warning">${word}</span>`
+        : word;
+    });
+    const highlightedText = highlightedWordsArray.join(" ");
+    textContainer.innerHTML = highlightedText;
+  } else {
+    textContainer.innerHTML = textContainer.textContent;
+  }
+});
